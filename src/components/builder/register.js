81ea872dc70e61ca4ac9293132a21ded6b797077
Robert Long <plong@laketrust.org>
2023-03-07 11:58:37 -0500

    Test Builder Component Folder Structure

--- src/components/builder/register.js
+++ src/components/builder/register.js
@@ -1,8 +1,5 @@
 import { Builder } from '@builder.io/react';
 import dynamic from 'next/dynamic';
-import MyCustomComponent from './MyCustomComponent';
-
-const config = [MyCustomComponent];
 
 Builder.registerComponent(
   dynamic(() => import('./MyCustomComponent')),
@@ -22,3 +19,12 @@ Builder.registerComponent(
     ],
   }
 );
+
+
+// Register a custom insert menu to organize your custom componnets
+// https://www.builder.io/c/docs/custom-components-visual-editor#:~:text=than%20this%20screenshot.-,organizing%20your%20components%20in%20custom%20sections,-You%20can%20create
+Builder.register('insertMenu', {
+    name: 'My Components',
+    items: [{ item: 'ExampleCustomComponent', name: 'My React Component' }],
+  });
+  
